b99217416e0219c00a77c256ec1c01cf2c0774b0
Ramshreyas rao <ram@mygram.me>
2015-06-23 15:02:46 +0530

    Incremental.

--- clojure-parser/src/clojure_parser/core.clj
+++ clojure-parser/src/clojure_parser/core.clj
@@ -1,15 +1,35 @@
 (ns clojure-parser.core
   (:gen-class))
 
+(defn parse-arguments [cd]
+  (if (= \[ (first cd))
+    (loop [code cd
+           arguments [""]]
+      (if (= \] (first code))
+        [arguments (rest code)]
+        (do
+          (println "args: " arguments)
+          (recur (rest code) [(str (get arguments 0) (first code))]))))
+    [nil code]))
+
+(defn parse-space [code]
+  (if (= \space (first code))
+    [\space (apply str (rest code))]
+    [nil code]))
+
+(defn parse [exp cd]
+  (loop [expression exp code cd]
+    (if (empty? code) 
+      expression
+      (recur {:node (str (:node expression) (first code))} 
+             (apply str (rest code))))))
+
+
 (defn -main
   "Clojure parser that returns an AST of the clojure code passed to it."
   ([path]
-   (-main [] [] (slurp path)))
+   (-main [] {:node ""} (slurp path)))
   ([tree node code]
    (if (empty? code)
-     (for [x node] (println x))
-     (conj tree (if (= \( (first code))
-                  (-main tree [] (rest code))
-                  (if (= \) (first code))
-                    node
-                    (-main tree (conj node (first code)) (rest code)))
+     tree
+     (conj tree (parse node code)))))
