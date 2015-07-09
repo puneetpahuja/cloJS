b114e74e780b3496b02775fe74fc130c65fbbd7e
Ramshreyas rao <ram@mygram.me>
2015-07-09 14:45:10 +0530

    Incremental commit.

--- clojure-parser/src/clojure_parser/interpreter.clj
+++ clojure-parser/src/clojure_parser/interpreter.clj
@@ -6,11 +6,11 @@
 (defn forms [form]
   (form
     {:def :def
-    :plus +
-    :minus -
-    :multiply *
-    :divide /
-    :equals =}))
+    :plus "+"
+    :minus "-"
+    :multiply "*"
+    :divide "/"
+    :equals "="}))
 
 (defmacro definate [fn-name args & body]
   (let [name (symbol (name fn-name))]
@@ -23,18 +23,19 @@
   (vec (map #(symbol (name %)) (:vector vector-map))))
 
 (defn express [expression-map]
-  (list (forms (first (keys expression-map))) 
-        (for [arg (first (vals expression-map))] arg)))
+  (concat (list (symbol (name (forms (first (keys expression-map))))))
+          (for [arg (first (vals expression-map))] (symbol (name arg)))))
 
 (defn evaluate [expression]
   (let [func (:form expression)
         args (:args expression)]
-    (cond
-      (= :def func) (println (first args) 
-                             (vectorate (second args))
-                             (express (last args)))
-      (= :fn func) (lambdinate args)
-      :else (println expression))))
+    (if (= :def func) 
+      (list (first args)
+        (vectorate (second args))
+        (express (last args)))
+      (if (= :fn func) 
+        (lambdinate args)
+        (println expression)))))
 
 (defn interpret-expression [exp]
   (let [result (first (parser/parse-expression exp))]
