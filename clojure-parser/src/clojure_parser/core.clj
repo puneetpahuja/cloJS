34b04a7f2889f25a6ee3500553d62b11beb4ca63
Ramshreyas rao <ram@mygram.me>
2015-09-02 12:48:50 +0530

    Gets evalate to correctly expand the tree structure to list form.

--- clojure-parser/src/clojure_parser/core.clj
+++ clojure-parser/src/clojure_parser/core.clj
@@ -367,7 +367,7 @@
            evaluated-args []]
       (let [arg (first args)]
         (if (empty? args)
-          (apply list func evaluated-args)
+          (apply list (symbol (name func)) evaluated-args)
           (if (map? arg)
             (recur (rest args) (conj evaluated-args (evalate arg)))
             (recur (rest args) (conj evaluated-args arg))))))))
